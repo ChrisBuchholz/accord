9b534358a192b69282f03f44a6856ca36873d8f9
tcmal <oscar.shrimpton.personal@gmail.com>
2018-06-16 17:05:45 +0100

    Added alphanumeric & alphanumeric_dashes validators

--- src/validators.rs
+++ src/validators.rs
@@ -141,6 +141,16 @@ pub fn contain_only(accepted: &'static [char]) -> Box<Fn(&String) -> ::Validator
     })
 }
 
+/// Convenience function; 0-9, A-z
+pub fn alphanumeric() -> Box<Fn(&String) -> ::ValidatorResult> {
+	contain_only(&['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+}
+
+/// Convenience function; Alphanumeric & underscore.
+pub fn alphanumeric_dashes() -> Box<Fn(&String) -> ::ValidatorResult> {
+	contain_only(&['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_', '-'])
+}
+
 /// Enforce that a string must not contain `needle`.
 pub fn not_contain(needle: &'static str) -> Box<Fn(&String) -> ::ValidatorResult> {
     Box::new(move |s: &String| {
